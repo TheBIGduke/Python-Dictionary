d14c4fd1dd6dc7494919a7bf766c06f800ebbf56
GonzaloUlises Martinez Vazquez <gonzalo.martinez@octopy.com>
2025-08-04 13:34:30 -0600

    Added the main logic of the interface

--- userInput.py
+++ userInput.py
@@ -1,1 +1,10 @@
 # This code is to save the user's info in a .JSON file and, then, store them in a Dictionary function to enable communication between two programs
+
+print("What's your name?")
+name = input("> ")
+
+print("How old are you?")
+age = input("> ")
+
+print("What's your height? (in cm)")
+height = input("> ")
